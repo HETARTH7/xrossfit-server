0b581d05308f0f72c62e0e0344bd3ae584c827f1
Hetarth <hetarth2028@gmail.com>
2023-12-29 12:33:04 +0530

    visualize exercize logs

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,10 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useAuthContext } from "../hooks/useAuthContext";
 import Navbar from "./Navbar";
+import LinePlot from "./ExerciseLineChart";
 
 const Home = () => {
   const { user } = useAuthContext();
-  
+  const [exerciseData, setExerciseData] = useState([]);
+
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch(`http://localhost:5000/data/${user.email}`, {
@@ -13,19 +15,42 @@ const Home = () => {
       const json = await response.json();
 
       if (response.ok) {
-        console.log(json);
+        const aggregatedData = aggregateExerciseData(json.exerciseLog);
+        setExerciseData(aggregatedData);
       }
     };
-    
 
     if (user) {
       fetchData();
     }
   }, [user]);
 
+  const aggregateExerciseData = (exerciseLog) => {
+    const dateCountMap = {};
+    exerciseLog.forEach((record) => {
+      const date = record.date;
+
+      if (dateCountMap[date]) {
+        dateCountMap[date] += 1;
+      } else {
+        dateCountMap[date] = 1;
+      }
+    });
+
+    const aggregatedData = Object.entries(dateCountMap).map(
+      ([date, count]) => ({
+        date,
+        count,
+      })
+    );
+
+    return aggregatedData;
+  };
+
   return (
     <div>
       <Navbar />
+      <LinePlot data={exerciseData} />
     </div>
   );
 };
