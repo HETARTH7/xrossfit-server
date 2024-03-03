1a13e00fbf5966c12928a3e5f7fdf932dae6864e
Hetarth <hetarth2028@gmail.com>
2024-03-03 13:52:50 +0530

    added dev api url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,5 +1,12 @@
 import axios from "axios";
 
+let url;
+if (process.env.API_URL) {
+  url = process.env.API_URL;
+} else {
+  url = "http://localhost:5000";
+}
+
 export default axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: url,
 });
